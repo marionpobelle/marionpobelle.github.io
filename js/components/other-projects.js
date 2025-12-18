3256c79676ed90010bb5169c068b5b0b7ffbcccb
marionpobelle <marion.pobelle@hotmail.com>
2025-12-18 17:22:39 +0100

    Added scroll to other projects overflow

--- js/components/other-projects.js
+++ js/components/other-projects.js
@@ -1,10 +1,10 @@
 var html = 
 `
 <button id='left-project-button' class="next-project-button">◄</button>
-<div style="width: 50%; height: 100%; display: flex; justify-content: center; align-items: center;">
+<div style="width: 50%; height: 100%; display: flex; justify-content: center; align-items: center; overflow: auto;">
     <p id="left-project" class="next-project-title">Project1</p>
 </div>
-<div style="width: 50%;  height: 100%; display: flex; justify-content: center; align-items: center;">
+<div style="width: 50%;  height: 100%; display: flex; justify-content: center; align-items: center; overflow: auto;">
     <p id="right-project" class="next-project-title">Project2</p>
 </div>
 <button id='right-project-button' class="next-project-button next-project-button-right">►</button>
